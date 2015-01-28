b848fcef0079720d4e0dabec8da02e3ddc728554
Nicholas Mascia <nmmascia@gmail.com>
2015-01-28 11:56:09 -0500

    Declare video capture.

--- client/templates/bets/video/_video.js
+++ client/templates/bets/video/_video.js
@@ -1,6 +1,9 @@
 Template._video.events({
   'click .record-button' : function(){
-
+    navigator.device.capture.captureVideo(captureSuccess)
   }
 });
 
+var captureSuccess = function(media){
+  console.log("Hello");
+}
